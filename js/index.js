3a089b7bdbad70bccb7cdbc335362295d2a30de4
ebertmota <ebertmota03@gmail.com>
2019-08-29 09:47:14 -0300

    armazenando tema atual no localStorage

--- js/index.js
+++ js/index.js
@@ -20,8 +20,28 @@ function calculate() {
     }
 }
 
+
+// ------------------ Armazenando tema no LocalStorage ------------------- //
+
+var gradientPink = "gradient-pink";
+var gradientBlue = "gradient-blue";
 const themeButton = document.getElementById('title');
 const container = document.querySelector('main');
-function toggleTheme() {
+
+
+function toggleTheme() {                                        //  troca o tema com o click no botao Js calculator
     container.classList.toggle("gradient-pink");
+}
+
+container.addEventListener("click",function setDefaultTheme() {
+    var currentPinkTheme = container.classList.contains("gradient-pink");
+    if (currentPinkTheme) {                                     //  se o container tiver a classe gradient-pink
+        localStorage.setItem('currentTheme', gradientPink );    //  ele guarda essa classe no localStorage
+    }else {                                                     //  se nao tiver ou for trocada
+        localStorage.setItem('currentTheme', gradientBlue);     //  ele guarda a classe gradient-azul no localStorage
+    }
+})
+
+if (localStorage.currentTheme){                                 //  se a chave currentTheme existir no LocalStorage
+    container.classList.add(localStorage.currentTheme);         //  ele coloca o valor dela como tema do container
 }
